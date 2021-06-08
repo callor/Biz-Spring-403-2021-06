e76b81efc76c86b0b721b2e5b4c7ab1469e7361d
callor <callor@callor.com>
2021-06-08 10:00:15 +0900

    Service

--- SpMVC_020_JDBC/src/main/java/com/callor/jdbc/controller/HomeController.java
+++ SpMVC_020_JDBC/src/main/java/com/callor/jdbc/controller/HomeController.java
@@ -1,39 +1,39 @@
 package com.callor.jdbc.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import com.callor.jdbc.pesistance.BookDao;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
+
+	/*
+	 * 보편적인 Spring에서 bean을 사용하는 코드
+	 * @Autowired
+	 * privatre BooDao bookdao
+	 * 이 코드에서 메모리 leak(누수)현상이 보고되어
+	 * 다음의 코드를 권장한다.
 	 */
+	protected final BookDao bookDao;
+	public HomeController(BookDao bookDao) {
+		this.bookDao = bookDao;
+	}
+	
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		bookDao.selectAll();
 		return "home";
+		
 	}
 	
 }
--- SpMVC_020_JDBC/src/main/java/com/callor/jdbc/pesistance/impl/BookDaoImplV1.java
+++ SpMVC_020_JDBC/src/main/java/com/callor/jdbc/pesistance/impl/BookDaoImplV1.java
@@ -2,13 +2,25 @@ package com.callor.jdbc.pesistance.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.callor.jdbc.model.BookVO;
 import com.callor.jdbc.pesistance.BookDao;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Repository("bookDaoV1")
 public class BookDaoImplV1 implements BookDao {
+
+	// Console로 log를 찍기 위하여 log 객체 생성	
+	// lombok @Slf4j 를 사용하여 아래 코드를 대신한다
+	//	private static Logger log 
+	//			= LoggerFactory.getLogger("SERVICE");
 	
 	// jdbc-context.xml 에 선언된 jdbcTemplate bean 사용하기
 	protected final JdbcTemplate jdbcTemplate;
@@ -20,9 +32,15 @@ public class BookDaoImplV1 implements BookDao {
 	public List<BookVO> selectAll() {
 		// TODO Auto-generated method stub
 		String sql = " SELECT * FROM tbl_books ";
+		/*
+		 * jdbcTemplate.query(sql,return type)
+		 * sql문을 실행한 후 return type형태로 데이터를
+		 * 변환하여 return 해달라
+		 */
 		List<BookVO> books = jdbcTemplate.query(sql,
 				new BeanPropertyRowMapper<BookVO>(BookVO.class)				
 		);
+		log.debug("SELECT {}",books.toString());
 		return null;
 	}
 
